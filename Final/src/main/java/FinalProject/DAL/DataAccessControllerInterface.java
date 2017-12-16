3df217f7f541cd779e4d5aa5e9fd97e913239f21
oded rojtblat <oded.rojtblat@gmail.com>
2017-12-16 12:01:11 +0200

    wrote DataAccessController

---
+++ Final/src/main/java/FinalProject/DAL/DataAccessControllerInterface.java
@@ -0,0 +1,15 @@
+package FinalProject.DAL;
+
+import FinalProject.BL.Agents.SmartHomeAgentBehaviour;
+import FinalProject.BL.Problems.Problem;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface DataAccessControllerInterface {
+
+    List<Problem> getProblems(List<String> problemNames);
+    List<String> getAvailableAlgorithms();
+    List<SmartHomeAgentBehaviour> getAlgorithms(List<String> algoNames);
+    void addAlgorithmToSystem(String path, String name) throws IllegalAccessException, IOException, InstantiationException;
+}
