558e800e738e8360859c91a743d4aba4514b8401
Unknown <guetaaviv@gmail.com>
2017-12-22 21:42:17 +0200

    added funcs to IterationAgentsPrice class

--- Final/src/main/java/FinalProject/BL/DataCollection/IterationAgentsPrice.java
+++ Final/src/main/java/FinalProject/BL/DataCollection/IterationAgentsPrice.java
@@ -1,6 +1,7 @@
 package FinalProject.BL.DataCollection;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,5 +10,34 @@ public class IterationAgentsPrice {
 
     public IterationAgentsPrice() {
         iterationToAgentsPrice = new HashMap<Integer, List<AgentPrice>>();
+    }
+
+    public Map<Integer, List<AgentPrice>> getIterationToAgentsPrice() {
+        return iterationToAgentsPrice;
+    }
+
+    public void setIterationToAgentsPrice(Map<Integer, List<AgentPrice>> iterationToAgentsPrice) {
+        this.iterationToAgentsPrice = iterationToAgentsPrice;
+    }
+
+    public List<AgentPrice> getAgentsPrices(int iterationNum){
+        return iterationToAgentsPrice.get(iterationNum);
+    }
+
+    public boolean isIterationOver(int iterationNum, int numOfAgents){
+        List<AgentPrice> agentsPrices = iterationToAgentsPrice.get(iterationNum);
+        if (agentsPrices != null){return agentsPrices.size() == numOfAgents;}
+        return false;
+    }
+
+    public void addAgentPrice(int iterationNum, AgentPrice agentPrice ){
+        if (iterationToAgentsPrice.containsKey(iterationNum)){
+            iterationToAgentsPrice.get(iterationNum).add(agentPrice);
+        }
+        else{
+            List<AgentPrice> agentsPrices = new LinkedList<AgentPrice>();
+            agentsPrices.add(agentPrice);
+            iterationToAgentsPrice.put(iterationNum, agentsPrices);
+        }
     }
 }
