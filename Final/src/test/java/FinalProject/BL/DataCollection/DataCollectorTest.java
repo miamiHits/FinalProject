59b56d25b56798fe93dbfcd786a36236cc675173
Unknown <guetaaviv@gmail.com>
2017-12-23 12:42:25 +0200

    remove avgPrice class, add DataCollectorTest

---
+++ Final/src/test/java/FinalProject/BL/DataCollection/DataCollectorTest.java
@@ -0,0 +1,16 @@
+package FinalProject.BL.DataCollection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataCollectorTest {
+    @Test
+    public void addData() throws Exception {
+    }
+
+    @Test
+    public void getNumOfAgentsInProblem() throws Exception {
+    }
+
+}
