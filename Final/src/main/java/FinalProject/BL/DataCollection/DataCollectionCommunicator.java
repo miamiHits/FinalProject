cd482477aa66c713f3f690c4e1c6d7e1cbf06979
Unknown <guetaaviv@gmail.com>
2017-12-27 17:59:06 +0200

    register and deregister the agent

--- Final/src/main/java/FinalProject/BL/DataCollection/DataCollectionCommunicator.java
+++ Final/src/main/java/FinalProject/BL/DataCollection/DataCollectionCommunicator.java
@@ -2,6 +2,10 @@ package FinalProject.BL.DataCollection;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
@@ -16,16 +20,41 @@ public class DataCollectionCommunicator extends Agent {
 
     @Override
     protected void setup() {
-        while (true) {
-            System.out.println("Agent " + getLocalName() + ": waiting for REQUEST message...");
-            ACLMessage msg = blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
-            System.out.println("Agent " + getLocalName() + ": REQUEST message received. Reply and exit.");
-            ACLMessage reply = new ACLMessage(ACLMessage.INFORM);
-            reply.addReceiver(msg.getSender());
-            msg.setOntology("XXXXX");
-            reply.setContent("exiting");
-            send(reply);
-            doDelete();
+        // Register the book-selling service in the yellow pages
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("dataCollector");
+        sd.setName("DataCollectionCommunicator");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
         }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        System.out.println("Agent " + getLocalName() + ": waiting for REQUEST message...");
+        ACLMessage msg = blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
+        System.out.println("Agent " + getLocalName() + ": REQUEST message received. Reply and exit.");
+        ACLMessage reply = new ACLMessage(ACLMessage.INFORM);
+        reply.addReceiver(msg.getSender());
+        msg.setOntology("XXXXX");
+        reply.setContent("exiting");
+        send(reply);
+        doDelete();
     }
+
+    protected void takeDown() {
+        // Deregister from the yellow pages
+        try {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        // Printout a dismissal message
+        System.out.println("DataCollectionCommunicator "+getAID().getName()+" terminating.");
+    }
+
 }
