9aa6233ac7f79543073e3c15e63e5c598dfbabab
Unknown <guetaaviv@gmail.com>
2017-12-27 18:35:59 +0200

    added agent to behaviour and use dataCollector

--- Final/src/main/java/FinalProject/BL/DataCollection/DataCollectionCommunicatorBehaviour.java
+++ Final/src/main/java/FinalProject/BL/DataCollection/DataCollectionCommunicatorBehaviour.java
@@ -1,9 +1,13 @@
 package FinalProject.BL.DataCollection;
 
+import FinalProject.BL.IterationData.IterationCollectedData;
+import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
 
 public class DataCollectionCommunicatorBehaviour extends CyclicBehaviour {
+    private DataCollectionCommunicator agent;
 
     @Override
     public void action() {
@@ -12,7 +16,14 @@ public class DataCollectionCommunicatorBehaviour extends CyclicBehaviour {
             // Message received. Process it
             String title = msg.getContent();
             ACLMessage reply = msg.createReply();
-            //reply.setPerformative(ACLMessage.PROPOSE);
+            try{
+               IterationCollectedData ICD = (IterationCollectedData)msg.getContentObject();
+               agent.collector.addData(ICD);
+            }catch(UnreadableException e){
+                //todo
+            }
+
+        //reply.setPerformative(ACLMessage.PROPOSE);
             //reply.setContent(String.valueOf(price.intValue()));
             myAgent.send(reply);
         }else{
