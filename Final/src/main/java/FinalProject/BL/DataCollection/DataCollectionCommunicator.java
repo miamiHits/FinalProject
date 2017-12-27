e7b2c67bd2ec9aaf8102f36b57198c1c0c8b28e1
Unknown <guetaaviv@gmail.com>
2017-12-27 16:57:22 +0200

    added setup func to communicator

--- Final/src/main/java/FinalProject/BL/DataCollection/DataCollectionCommunicator.java
+++ Final/src/main/java/FinalProject/BL/DataCollection/DataCollectionCommunicator.java
@@ -2,6 +2,8 @@ package FinalProject.BL.DataCollection;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class DataCollectionCommunicator extends Agent {
 
@@ -10,7 +12,15 @@ public class DataCollectionCommunicator extends Agent {
 
     @Override
     protected void setup() {
-        super.setup();
-
+        while (true) {
+            System.out.println("Agent " + getLocalName() + ": waiting for REQUEST message...");
+            ACLMessage msg = blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
+            System.out.println("Agent " + getLocalName() + ": REQUEST message received. Reply and exit.");
+            ACLMessage reply = new ACLMessage(ACLMessage.INFORM);
+            reply.addReceiver(msg.getSender());
+            reply.setContent("exiting");
+            send(reply);
+            doDelete();
+        }
     }
 }
