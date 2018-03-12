6de6c8891dfaf88b030995238213de164395c345
gal tfilin <tfiling@post.bgu.ac.il>
2018-03-12 21:20:45 +0200

    applying an OS based path separator

--- src/main/java/FinalProject/App.java
+++ src/main/java/FinalProject/App.java
@@ -4,14 +4,21 @@ import FinalProject.DAL.*;
 import FinalProject.Service;
 import FinalProject.PL.UiHandler;
 
+import java.io.File;
+
 public class App
 {
     public static void main( String[] args )
     {
         org.apache.log4j.BasicConfigurator.configure();
 
-        JsonLoaderInterface jsonLoader = new JsonLoader("Final\\src\\test\\testResources\\jsons");
-        AlgoLoaderInterface algorithmLoader = new AlgorithmLoader("Final\\target\\classes\\FinalProject\\BL\\Agents");
+        String jsonPath = "src/test/testResources/jsons";
+        jsonPath.replaceAll("/", File.separator);
+        String algorithmsPath = "target/classes/FinalProject/BL/Agents";
+        algorithmsPath.replaceAll("/", File.separator);
+
+        JsonLoaderInterface jsonLoader = new JsonLoader(jsonPath);
+        AlgoLoaderInterface algorithmLoader = new AlgorithmLoader(algorithmsPath);
         DataAccessController dal = new DataAccessController(jsonLoader, algorithmLoader);
         Service service = new Service(dal);
         UiHandler ui = new UiHandler(service);
