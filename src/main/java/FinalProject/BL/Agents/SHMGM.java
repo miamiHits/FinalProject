4a5a50ca19ab81c054254ab2858e3893948c43cf
Oded <oded.rojtblat@gmail.com>
2018-03-20 10:43:51 +0200

    started work on shmgm

---
+++ src/main/java/FinalProject/BL/Agents/SHMGM.java
@@ -0,0 +1,50 @@
+package FinalProject.BL.Agents;
+
+import org.apache.log4j.Logger;
+
+import java.util.Map;
+
+public class SHMGM extends SmartHomeAgentBehaviour{
+
+    private final static Logger logger = Logger.getLogger(SHMGM.class);
+
+    @Override
+    protected void doIteration() {
+        if (agent.isZEROIteration()) {
+            logger.info("Starting work on Iteration: 0");
+            buildScheduleFromScratch();
+            agent.setZEROIteration(false);
+            logger.info("FINISH ITER 0");
+        }
+        else {
+            //TODO: other iter here
+        }
+        beforeIterationIsDone();
+        this.currentNumberOfIter++;
+    }
+
+    @Override
+    protected void onTermination() {
+
+    }
+
+    @Override
+    protected long countIterationCommunication() {
+        return 0;
+    }
+
+    @Override
+    protected void generateScheduleForProp(PropertyWithData prop, double ticksToWork, Map<String, Double> sensorsToCharge) {
+
+    }
+
+    @Override
+    public DSA cloneBehaviour() {
+        DSA newInstance = new DSA();
+        newInstance.finished = this.finished;
+        newInstance.currentNumberOfIter = this.currentNumberOfIter;
+        newInstance.FINAL_TICK = this.FINAL_TICK;
+        newInstance.agentIterationData = null; //will be created as part of the behaviour run(see buildScheduleFromScratch)
+        return newInstance;
+    }
+}
