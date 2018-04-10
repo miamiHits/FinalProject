8399a00cf611e89a55f971b1bf37bb28940516a2
Oded <oded.rojtblat@gmail.com>
2018-04-10 11:33:19 +0300

    addded a few tests in smab

--- src/test/FinalProject/BL/Agents/SmartHomeAgentBehaviourTest.java
+++ src/test/FinalProject/BL/Agents/SmartHomeAgentBehaviourTest.java
@@ -1,10 +1,14 @@
 package FinalProject.BL.Agents;
 
 import FinalProject.BL.DataObjects.Problem;
+import FinalProjectTests.BL.Agents.ReflectiveUtils;
 import FinalProjectTests.DAL.DalTestUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import sun.misc.GC;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +16,7 @@ public class SmartHomeAgentBehaviourTest {
 
     private SmartHomeAgent agent;
     private Problem dm_7_1_2;
+    private SmartHomeAgentBehaviour smab;
 
     @Before
     public void setUp() throws Exception {
@@ -20,11 +25,17 @@ public class SmartHomeAgentBehaviourTest {
 
         //create a problem obj
         dm_7_1_2 = DalTestUtils.getProblemDm_7_1_2();
-        agent = new SmartHomeAgent();
+        agent = ReflectiveUtils.initSmartHomeAgentForTest(dm_7_1_2);
+        smab = new DSA(agent);
+        smab.initializeBehaviourWithAgent(agent);
+
     }
 
     @After
     public void tearDown() throws Exception {
+        dm_7_1_2 = null;
+        smab = null;
+        agent = null;
     }
 
     @Test
@@ -41,7 +52,11 @@ public class SmartHomeAgentBehaviourTest {
 
     @Test
     public void drawRandomNumIsInRangeTest() {
-
+        int low = 0;
+        int high = 100;
+        int res = smab.drawRandomNum(low, high);
+        Assert.assertTrue(res >= low);
+        Assert.assertTrue(res <= high);
     }
 
     @Test
@@ -49,7 +64,15 @@ public class SmartHomeAgentBehaviourTest {
     }
 
     @Test
-    public void flipCoin() {
+    public void flipCoinTestZeroProb() {
+        boolean shouldBeFalse = smab.flipCoin(0);
+        Assert.assertFalse(shouldBeFalse);
+    }
+
+    @Test
+    public void flipCoinTestOneProb() {
+        boolean shouldBeTrue = smab.flipCoin(1);
+        Assert.assertTrue(shouldBeTrue);
     }
 
     @Test
@@ -65,10 +88,34 @@ public class SmartHomeAgentBehaviourTest {
     }
 
     @Test
-    public void calcPrice() {
+    public void calcPriceTest() {
+        double expected = 3.837;
+        double[] allOnesSched = getHorizonSizedArrWithSameVals(1);
+        double res = smab.calcPrice(allOnesSched);
+        Assert.assertEquals(expected, res, 0);
     }
 
     @Test
-    public void addBackgroundLoadToPowerConsumption() {
+    public void calcPriceTestAllZeros() {
+        double expected = 0;
+        final int val = 0;
+        double[] allZerosSched = getHorizonSizedArrWithSameVals(val);
+        double res = smab.calcPrice(allZerosSched);
+        Assert.assertEquals(expected, res, 0);
+    }
+
+    private double[] getHorizonSizedArrWithSameVals(int val) {
+        double[] arr = new double[dm_7_1_2.getHorizon()];
+        for (int i = 0; i < dm_7_1_2.getHorizon(); i++) {
+            arr[i] = val;
+        }
+        return arr;
+    }
+
+    @Test
+    public void addBackgroundLoadToPowerConsumptionTest() {
+        double[] allZerosSched = getHorizonSizedArrWithSameVals(0);
+        smab.addBackgroundLoadToPowerConsumption(allZerosSched);
+        Assert.assertArrayEquals(dm_7_1_2.getAgentsData().get(0).getBackgroundLoad(), allZerosSched, 0);
     }
 }
