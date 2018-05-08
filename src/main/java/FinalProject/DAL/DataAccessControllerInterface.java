75918338f40daa09fcc1bbcbed973c2f064821e5
Oded <oded.rojtblat@gmail.com>
2018-05-08 20:54:42 +0300

    now have 3 levels in tree

--- src/main/java/FinalProject/DAL/DataAccessControllerInterface.java
+++ src/main/java/FinalProject/DAL/DataAccessControllerInterface.java
@@ -5,13 +5,12 @@ import FinalProject.BL.AlgoAddResult;
 import FinalProject.BL.DataObjects.Problem;
 
 import java.util.List;
-import java.util.Map;
 
 public interface DataAccessControllerInterface {
 
     List<Problem> getProblems(List<String> problemNames);
     List<String> getAvailableAlgorithms();
-    Map<Integer, List<String>> getAvailableProblems();
+    List<String> getAvailableProblems();
     List<SmartHomeAgentBehaviour> getAlgorithms(List<String> algoNames);
     AlgoAddResult addAlgorithmToSystem(String path, String name);
 }
