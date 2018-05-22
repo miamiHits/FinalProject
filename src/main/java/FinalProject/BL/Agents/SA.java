6cce41ae1e3ee575c7c5133e586740a04adca9fb
Oded <oded.rojtblat@gmail.com>
2018-05-22 22:59:24 +0300

    started commenting

--- src/main/java/FinalProject/BL/Agents/SA.java
+++ src/main/java/FinalProject/BL/Agents/SA.java
@@ -10,12 +10,39 @@ import java.util.stream.Collectors;
 
 import static FinalProject.BL.DataCollection.PowerConsumptionUtils.calculateEPeak;
 
+/**
+ * This class implements the SA (Simulated Anealing) algorithm and is provided
+ * as is, as part of the system.
+ *
+ * The following is pseudo-code for the algorithm (including actions that are necessary for the work
+ * of the system but are not part of the algorithm per say, such as sending messages to the data collector):
+ *
+ * init: (iteration 0):
+ * generate a random schedule satisfying the constraints
+ * send iteration data to collector
+ * send schedule to all neighbours
+ *
+ * iterations (iteration > 0):
+ * receive schedule from all neighbours
+ * build tick subsets for all properties (if not built already)
+ * pick a random subset for each property
+ * if the new schedule improves the total grade (calculation including Csum and Epeak, see calcImproveOptionGrade):
+ *      switch to new schedule
+ * else:
+ *      switch to the new schedule with probability 1 - (current number of iteration / total number of iterations)
+ * send iteration data to collector
+ * send schedule to all neighbours
+ */
 public class SA extends SmartHomeAgentBehaviour{
 
     private final static Logger logger = Logger.getLogger(SA.class);
 
     public SA() { super(); }
 
+    /**
+     * Overridden from {@link SmartHomeAgentBehaviour}.
+     * This is the main methods that runs SA specific logic
+     */
     @Override
     protected void doIteration() {
         if (agent.isZEROIteration()) {
@@ -35,12 +62,23 @@ public class SA extends SmartHomeAgentBehaviour{
         this.currentNumberOfIter++;
     }
 
+    /**
+     * Wait for messages from all neighbours (sent in previous iteration),
+     * read the messages into agent.getMyNeighborsShed,
+     * calculate the power consumption for all neighbours
+     */
     private void receiveAllMessagesAndHandleThem() {
         List<ACLMessage> messageList = waitForNeighbourMessages(SmartHomeAgent.MESSAGE_TEMPLATE_SENDER_IS_NEIGHBOUR);
         readNeighboursMsgs(messageList);
         helper.calcPowerConsumptionForAllNeighbours();
     }
 
+    /**
+     * Pick a random schedule for each property (out of the pre-calculated subsets),
+     * calculate the total grade of this schedule,
+     * compare to total grade of previous schedule.
+     * Switch to the new schedule if it is better than previous schedule, or with probability (current number of iteration / total number of iterations)
+     */
     private void pickAndApplyRandomSched() {
         List<double[]> allScheds = agent.getMyNeighborsShed().stream()
                 .map(AgentIterationData::getPowerConsumptionPerTick)
@@ -58,9 +96,6 @@ public class SA extends SmartHomeAgentBehaviour{
         });
         allScheds.add(prevSched);
         double prevGrade = calcImproveOptionGrade(prevSched, allScheds);
-        if (agent.getLocalName().equals("h1")) {
-            logger.info("H1 iter: " + currentNumberOfIter + ", prevGrade: " + prevGrade);
-        }
 
         allScheds.remove(prevSched);
         Map<PropertyWithData, Set<Integer>> randomSchedForAllProps = new HashMap<>(propToSubsetsMap.size());
@@ -75,9 +110,6 @@ public class SA extends SmartHomeAgentBehaviour{
         });
         allScheds.add(randSched);
         double newGrade = calcImproveOptionGrade(randSched, allScheds);
-        if (agent.getLocalName().equals("h1")) {
-            logger.info("H1 iter: " + currentNumberOfIter + ", newGrade: " + newGrade);
-        }
 
         if (newGrade < prevGrade || shouldTakeNewSched()) {
             helper.totalPriceConsumption = newGrade;
@@ -99,11 +131,21 @@ public class SA extends SmartHomeAgentBehaviour{
         }
     }
 
+    /**
+     * @return whether or not to switch to a new schedule
+     * which is not better than the previous one (called from pickAndApplyRandomSched).
+     */
     private boolean shouldTakeNewSched() {
         float probability = calcProbabilityToTakeNewSched();
         return flipCoin(probability);
     }
 
+    /**
+     * Calculate the probability to take a schedule which is not better than the previous one.
+     * The probability is defined by: 1 - (current number of iteration / total number of iterations)
+     * @return -1 if the current number of iteration is <= 0
+     * else, 1 - (current number of iteration / total number of iterations).
+     */
     private float calcProbabilityToTakeNewSched() {
         if (agent.getAgentData().getNumOfIterations() <= 0 || currentNumberOfIter <= 0) {
             logger.info("Num of iter <= 0 or currentNumberOfIter <= 0!");
@@ -113,6 +155,13 @@ public class SA extends SmartHomeAgentBehaviour{
         return 1 - ((float) currentNumberOfIter / agent.getAgentData().getNumOfIterations());
     }
 
+    /**
+     * Pick a random subset of ticks to work in for property prop
+     * out of all the possible subsets for it.
+     * @param prop The {@link PropertyWithData} to which a random subset of ticks should be chosen.
+     * @return A random set of ticks for {@link PropertyWithData} prop to work in.
+     * The ticks satisfy all constraints for this property.
+     */
     private Set<Integer> pickRandomSubsetForProp(PropertyWithData prop) {
         List<Set<Integer>> allSubsets = propToSubsetsMap.get(prop);
         if (allSubsets == null || allSubsets.isEmpty()) {
@@ -122,46 +171,44 @@ public class SA extends SmartHomeAgentBehaviour{
         return allSubsets.get(index);
     }
 
+    /**
+     * Overridden from {@link SmartHomeAgentBehaviour}.
+     * In this algorithm, this methods works a bit differently to other algorithms (see {@link DSA}, {@link SHMGM}, etc.).
+     * If currentNumberOfIter == 0, build a schedule for {@link PropertyWithData} prop (same as other algorithms),
+     * else, generate subsets for prop if not already generated.
+     * @param prop the property to which the schedule should be generated
+     * @param ticksToWork number of active ticks needed
+     * @param sensorsToCharge sensors affected
+     * @param randomSched ignored in this algorithm.
+     */
     @Override
     protected void generateScheduleForProp(PropertyWithData prop, double ticksToWork, Map<String, Integer> sensorsToCharge, boolean randomSched) {
         if (agent.isZEROIteration()) {
             startWorkZERO(prop, sensorsToCharge, ticksToWork);
         }
         else {
-//            startWorkNonZeroIter(prop, sensorsToCharge, ticksToWork, randomSched);
             if (!propToSubsetsMap.containsKey(prop)) {
                 getSubsetsForProp(prop, ticksToWork); //to put in map if absent
             }
-//            addSensorsToChargeForPropIfAbsent(prop, sensorsToCharge);
         }
     }
 
-//    private void addSensorsToChargeForPropIfAbsent(PropertyWithData prop, Map<String, Integer> sensorsToCharge) {
-//        propToSensorsToChargeMap.putIfAbsent(prop,sensorsToCharge);
-//    }
-//
-//    private List<Set<Integer>> initSubsetsForProp(PropertyWithData prop, double ticksToWork) {
-//        List<Set<Integer>> subsets;
-//        if (ticksToWork <= 0) {
-//            subsets = checkAllSubsetOptions(prop);
-//            if (subsets == null ) {
-//                logger.warn("subsets is null!");
-//            }
-//        }
-//        else {
-//            List<Integer> rangeForWork = calcRangeOfWork(prop);
-//            subsets = helper.getSubsets(rangeForWork, (int) ticksToWork);
-//        }
-//        propToSubsetsMap.put(prop, subsets);
-//        return subsets;
-//    }
-
+    /**
+     * Overridden from {@link SmartHomeAgentBehaviour}.
+     * This method is called just before this agent is terminated (when the run has finished
+     * for this (algorithm, problem) pair. In this algorithm, it only prints to log.
+     */
     @Override
     protected void onTermination() {
         logger.info(agent.getName() + " for problem " +
                 agent.getProblemId() + "and algo SA is TERMINATING!");
     }
 
+    /**
+     * Count the number and total size of messages sent by this agent
+     * during this iteration. This is important for data collection
+     * and not for the actual run of the algorithm.
+     */
     @Override
     protected void countIterationCommunication() {
         int count = 0;
@@ -186,6 +233,9 @@ public class SA extends SmartHomeAgentBehaviour{
         }
     }
 
+    /**
+     * @return A deep copy of this instance.
+     */
     @Override
     public SA cloneBehaviour() {
         SA newInstance = new SA();
